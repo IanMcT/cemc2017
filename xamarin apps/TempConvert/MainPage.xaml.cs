3b463db13f8d57849cf0ddf09cab045888c25233
IanMcT <ian.mctavish@tldsb.on.ca>
2017-08-04 09:34:36 -0400

    Add files via upload
    The xaml and cs files that are used in a temperature conversion app - note these are the only two files that need to be edited to create a fully functioning app.

---
+++ xamarin apps/TempConvert/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace TempConvert
+{
+	public partial class MainPage : ContentPage
+	{
+        Boolean celsiusToFahrenheit = true;
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Picker p = (Picker)sender;
+            if (p.SelectedItem.ToString() == "Celsius to Fahrenheit")
+            {
+                celsiusToFahrenheit = true;
+            }
+            else
+            {
+                celsiusToFahrenheit = false;
+            }
+            Debug.WriteLine(p.SelectedItem);
+        }
+
+        private void btnConvert_Clicked(object sender, EventArgs e)
+        {
+            double temp;
+
+            double.TryParse(entryTemperature.Text, out temp);
+            if (celsiusToFahrenheit)
+            {
+                lblOutput.Text = (temp * 1.8 + 32).ToString() + " degrees Fahrenheit";
+            }
+            else
+            {
+                lblOutput.Text = (((temp - 32) * 5) / 9).ToString() + " degrees Celsius";
+            }
+        }
+    }
+}
